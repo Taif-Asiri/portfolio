b84a7123b3ac0fa5dbd99ff231fd2c9f01e578c9
Taif-Asiri <toti4438@gmail.com>
2025-09-11 09:28:21 +0300

    initial commit

---
+++ scripts/app.js
@@ -0,0 +1,8 @@
+const toggleBtn = document.getElementById("mode-toggle");
+const body = document.body;
+
+toggleBtn.addEventListener("click", () => {
+  body.classList.toggle("dark");
+  toggleBtn.textContent = body.classList.contains("dark") ? "☀️" : "🌙";
+});
+
