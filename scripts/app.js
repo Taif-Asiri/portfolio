cd3f2043110c094212d28f9e8e377b3f1e78bfce
Taif-Asiri <toti4438@gmail.com>
2025-09-14 08:56:28 +0300

    Submitting the portfolio project

--- scripts/app.js
+++ scripts/app.js
@@ -1,2 +1,28 @@
+ const toggleBtn = document.getElementById("mode-toggle");
+const body = document.body;
+
+toggleBtn.addEventListener("click", () => {
+  body.classList.toggle("dark");
+  toggleBtn.textContent = body.classList.contains("dark") ? "Light mode☀️" : "Dark mode🌙";
+});
+const buttons = document.querySelectorAll(".read-more-btn");
+
+buttons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const card = btn.parentElement;
+    const moreText = card.querySelector(".more-text");
+
+    moreText.classList.toggle("hidden");
+
+    if (moreText.classList.contains("hidden")) {
+      btn.textContent = "Read more";
+    } else {
+      btn.textContent = "Read less";
+    }
+  });
+});
+ // Toggle Dark Mode - MDN  Web Docs -Add Event Listner
+ // Read More Button - MDN  Web Docs  - Document querySelectorAll
+ 
 
 
